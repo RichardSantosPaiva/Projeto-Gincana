24725dc2fb30c8b50741c5ce229d30f55e3892a3
Blackout Momo <sergiogabriel2606g@outlook.com>
2023-11-29 16:48:01 -0300

    sql2: adicionando a instrução de idade

--- SistemaGincana/CadastroAluno.php
+++ SistemaGincana/CadastroAluno.php
@@ -5,33 +5,33 @@
     include_once("conectaBD.php");
     //tabela no banco de dados
     $tabela="alunos";
-    $campos="nome";
+
+    //define campos do insert
+    $campoNome="nome";
+    $campoIdade="idade";
 
 
     // se o botão for pressionado
-    if(isset($_GET['enviar'])){
-        $nome = $_GET['nomeAluno'];
-        //Script para inserir um registro na tabela no Banco de Dados
-        $sql = "INSERT INTO $tabela ($campos)
-                VALUES ('$nome')";
-                
-        //executando instrução sql
-
-        $instrucao = mysqli_query($conexao,$sql);
-        //concluindo operação
-
-        if (!$instrucao) {
-            die('Query inválida '.mysqli_error($conexao));
-        } else {
-            mysqli_close($conexao);
-
- 
-            echo "<a href='index.php' id='linkVoltar'>Voltar</a>";
-            echo "$nome adicionado com sucesso'";
-            exit;
-        
+        if(isset($_GET['enviar'])){
+            $nomeAluno = $_GET['nomeAluno'];
+            $idadeAluno = $_GET['idadeAluno'];
+            //Script para inserir um registro na tabela no Banco de Dados
+                $sql = "INSERT INTO $tabela ($campoNome)
+                        VALUES ('$nomeAluno')";
+                $sql2 = "INSERT INTO $tabela ($campoIdade)
+                VALUES ('$idadeAluno')";
+            //executando instrução sql
+            $instrucao = mysqli_query($conexao,$sql,$sql2);
+            //concluindo operação
+            if (!$instrucao) {
+                die('Query inválida '.mysqli_error($conexao));
+            } else {
+                mysqli_close($conexao);
+                echo "<a href='index.php' id='linkVoltar'>Voltar</a>";
+                exit;
+            }
         }
-    }
+
 ?>
 
 <!DOCTYPE html>
@@ -97,7 +97,7 @@ a:hover{
                     <input type="text" name="nomeAluno" class="input nome" placeholder="Nome completo">
                     <input type="number" name="idadeAluno" class="input idade" placeholder="sua idade" max="18" min="15">
                     <button name="enviar" type="submit">Enviar</button>
-                     <a href="index.php" id="linkVoltar">Voltar</a>
+
                 </div>
         </main>
 
