470d5877727e9ed0d4b390d29bde2ac3a6364640
Blackout Momo <sergiogabriel2606g@outlook.com>
2023-11-30 20:00:29 -0300

    mostrando ids,nomes e quase pontos

--- SistemaGincana/consultaPontos.php
+++ SistemaGincana/consultaPontos.php
@@ -9,19 +9,21 @@ o aluno ver a pontuação na tabela dele
 
 include_once("conectaBD.php");
 
-$tabela = "diasGincana";
-$campos = "dia";
 
 
 
-if (isset($GET['enviar'])) {
+
+
+if (isset($_GET['enviar'])) {
     $pontosAluno = $_GET['pontosAluno'];
-    $diaGincana = $_GET['diaGincana'];
-
+    $Aluno = $_GET['aluno'];
+    $dia = $_GET['dia'];
+    $tabela = "alunos";
+    $campos = "pontos";
     //Script para inserir um registro na tabela no Banco de Dados
 
-    $sql = "INSERT INTO $tabela ()
-        VALUES ()";
+    $sql = "INSERT INTO $tabela ($campos)
+        VALUES ('$pontosAluno')";
 
 
     //executando instrução sql
@@ -31,8 +33,7 @@ if (isset($GET['enviar'])) {
     if (!$instrucao) {
         die('Query inválida ' . mysqli_error($conexao));
     } else {
-        mysqli_close($conexao);
-        header('Location: index.php');
+
         // O header leva para a pagina inic
     }
 }
@@ -48,7 +49,7 @@ if (isset($GET['enviar'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema Gincana</title>
-    <link rel="stylesheet" href="css/tabela-pontos.css">    
+    <link rel="stylesheet" href="css/tabela-pontos.css">
     <link rel="stylesheet" href="css/layout.css">
     <link rel="stylesheet" href="css/forms.css">
 
@@ -94,17 +95,23 @@ if (isset($GET['enviar'])) {
                         $resultAluno = $conexao->query("SELECT * FROM alunos");
                         ?>
 
-                        <select>
+                        <select name="aluno">
                             <?php
-                            while ($row = $resultDia->fetch_assoc()) {
-                                echo "<option>" . $row['dia'] . "</option>";
-                            }
+                            while ($row = $resultAluno->fetch_assoc()):
+                                ?>
+
+                                <?php
+                                echo "<option value='" . $row['idAlunos'] . "'>" . $row['nome'] . "</option>";
+                                ?>
+
+                                <?php
+                            endwhile;
                             ?>
                         </select><br><br>
-                            <select>
+                        <select name="dia">
                             <?php
-                            while ($row = $resultAluno->fetch_assoc()) {
-                                echo "<option>" . $row['nome'] . "</option>";
+                            while ($row = $resultDia->fetch_assoc()) {
+                                echo "<option value='" . $row['idDiaGincana'] . "'>" . $row['dia'] . "</option>";
                             }
                             ?>
                         </select>
@@ -117,49 +124,71 @@ if (isset($GET['enviar'])) {
 
             <div id="pontos" class="background-red-pontos">
                 <div class="background-white-pontos">
-                     
+
                     <div id="tabela-pontos">
 
-                            <div id="pontos-alunos"><!-- comeco pontos alunos -->
-                                <h3>Alunos</h3>
+                        <div id="pontos-alunos"><!-- comeco pontos alunos -->
+                            <h3>Alunos</h3>
 
-                                <div id="titulo-tbl-alunos"> <!-- começo cabecalho aluno-->
-                                    <div>
-                                        <h4>Id</h4>
-                                    </div>
-                                    <div>
-                                        <h4>Nome</h4>
-                                    </div>
-                                    <div>
-                                        <h4>Pontos individual</h4>
-                                    </div>
-                                </div> <!-- final cabecalho aluno-->
-                                
-                                <div id="dados-alunos"> <!--comeco dados alunos -->
-                                    <div>
-                                        <p>id aluno</p>
-                                    </div>
-                                    <div>
-                                        <p>nome aluno</p>
-                                    </div>
-                                    <div>
-                                       <p>pontos aluno</p>
-                                    </div>
-                                </div><!-- final dados alunos-->
-
-                            </div><!--final ponto alunos -->
-
-                            <div id="pontos-turma"> <!-- comeco pontos turma-->
-                                <h3>Turma</h3>
-                               
-                                <div id="titulo-tbl-turma">
-                                    <h4>pontos total</h4>
+                            <div id="titulo-tbl-alunos"> <!-- começo cabecalho aluno-->
+                                <div>
+                                    <h4>Id</h4>
                                 </div>
-                                <div id="dados-turma">
-                                    <p>pontos total da turma</p>
+                                <div>
+                                    <h4>Nome</h4>
                                 </div>
+                                <div>
+                                    <h4>Pontos individual</h4>
+                                </div>
+                            </div> <!-- final cabecalho aluno-->
 
-                            </div> <!-- fnal pontos turma-->
+                            <div id="dados-alunos"> <!--comeco dados alunos -->
+                                <div>
+                                    <?php
+
+                                    $sqlAluno = "select * from $tabela";
+                                    $instrucaoAluno = mysqli_query($conexao, $sqlAluno);
+                                    foreach ($instrucaoAluno as $exibe) {
+                                        echo "<p>" . $exibe['idAlunos'] . "</p>";
+                                    }
+                                    ?>
+                                </div>
+                                <div>
+                                    <?php
+
+                                    $sqlAluno = "select * from $tabela";
+                                    $instrucaoAluno = mysqli_query($conexao, $sqlAluno);
+                                    foreach ($instrucaoAluno as $exibe) {
+                                        echo "<p>" . $exibe['nome'] . "</p>";
+                                    }
+                                    ?>
+                                </div>
+                                <div>
+                                    <p>pontos aluno</p>
+                                    <?php
+
+                                    $sqlAluno = "select * from $tabela";
+                                    $instrucaoAluno = mysqli_query($conexao, $sqlAluno);
+                                    foreach ($instrucaoAluno as $exibe) {
+                                        echo "<p>" . $exibe['pontos'] . "</p>";
+                                    }
+                                    ?>
+                                </div>
+                            </div><!-- final dados alunos-->
+
+                        </div><!--final ponto alunos -->
+
+                        <div id="pontos-turma"> <!-- comeco pontos turma-->
+                            <h3>Turma</h3>
+
+                            <div id="titulo-tbl-turma">
+                                <h4>pontos total</h4>
+                            </div>
+                            <div id="dados-turma">
+                                <p>pontos total da turma</p>
+                            </div>
+
+                        </div> <!-- fnal pontos turma-->
 
                     </div>
 
