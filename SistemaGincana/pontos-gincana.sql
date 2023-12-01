692196abb35d1097f932b92cce264782a9e73927
Blackout Momo <sergiogabriel2606g@outlook.com>
2023-12-01 14:27:30 -0300

    agora os pontos estão indo para os alunos

--- SistemaGincana/pontos-gincana.sql
+++ SistemaGincana/pontos-gincana.sql
@@ -1,49 +1,50 @@
-DROP DATABASE IF EXISTS  gincana;
-CREATE DATABASE gincana;
-USE gincana;
+    DROP DATABASE IF EXISTS  gincana;
+    CREATE DATABASE gincana;
+    USE gincana;
 
-CREATE TABLE diasGincana(
-	idDiaGincana INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    dia DATE
-);
+    CREATE TABLE diasGincana(
+        idDiaGincana INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        dia DATE
+    );
 
-CREATE TABLE turmas (
-    idTurma INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    turma VARCHAR(70) /*1info, ou 2info, ou 3 info,   1adm, ou 2 adm etc*/
-);
+    CREATE TABLE turmas (
+        idTurma INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        turma VARCHAR(70) /*1info, ou 2info, ou 3 info,   1adm, ou 2 adm etc*/
+        
+    );
 
-CREATE TABLE alunos (
-    idAlunos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    pontos INT NOT NULL,
-    nome VARCHAR (77) NOT NULL,
-    idade INT(2) NOT NULL,
-    idDiaGincana INT,
-    idTurma INT,
-     CONSTRAINT fk_aluno_diaGincana FOREIGN KEY (idDiaGincana )
-        REFERENCES diasGincana (idDiaGincana ),
-	 CONSTRAINT fk_aluno_turma FOREIGN KEY (idTurma)
-		REFERENCES turmas (idTurma)
-);
+    CREATE TABLE alunos (
+        idAlunos INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        pontos INT NOT NULL,
+        nome VARCHAR (77) NOT NULL,
+        idade INT(2) NOT NULL,
+        idDiaGincana INT,
+        idTurma INT,
+        CONSTRAINT fk_aluno_diaGincana FOREIGN KEY (idDiaGincana )
+            REFERENCES diasGincana (idDiaGincana ),
+        CONSTRAINT fk_aluno_turma FOREIGN KEY (idTurma)
+            REFERENCES turmas (idTurma)
+    );
 
-/*
-create table AlunosTurma (
-	idAlunosTurma INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    idTurma INT,
-    idAlunos INT,
-    CONSTRAINT fk_aluno_turma FOREIGN KEY (idTurma)
-        REFERENCES turmas (idTurma),
-	 CONSTRAINT fk_alunos_turma FOREIGN KEY (idAlunos)
-        REFERENCES alunos (idAlunos)
-);
-*/
+    /*
+    create table AlunosTurma (
+        idAlunosTurma INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+        idTurma INT,
+        idAlunos INT,
+        CONSTRAINT fk_aluno_turma FOREIGN KEY (idTurma)
+            REFERENCES turmas (idTurma),
+        CONSTRAINT fk_alunos_turma FOREIGN KEY (idAlunos)
+            REFERENCES alunos (idAlunos)
+    );
+    */
 
-/* Inserindo os dados na tabela turma */
-INSERT INTO turmas (turma) VALUES ('1º INFO');
-INSERT INTO turmas (turma) VALUES ('2º INFO');
-INSERT INTO turmas (turma) VALUES ('3º INFO');
-INSERT INTO turmas (turma) VALUES ('1º ADM');
-INSERT INTO turmas (turma) VALUES ('2º ADM');
-INSERT INTO turmas (turma) VALUES ('3º ADM');
+    /* Inserindo os dados na tabela turma */
+    INSERT INTO turmas (turma) VALUES ('1º INFO');
+    INSERT INTO turmas (turma) VALUES ('2º INFO');
+    INSERT INTO turmas (turma) VALUES ('3º INFO');
+    INSERT INTO turmas (turma) VALUES ('1º ADM');
+    INSERT INTO turmas (turma) VALUES ('2º ADM');
+    INSERT INTO turmas (turma) VALUES ('3º ADM');
 
 
- 
+    
